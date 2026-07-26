e8e24af3d379c0c1a1f83829f050789526dc283b
dishanchowdhury111-creator <dishanchowdhury111@gmail.com>
2026-07-26 11:35:40 +0530

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -25,10 +25,47 @@ const TOOL_FUNCTIONS = {
 
 function renderScientificCalculator(container){
 
-    container.innerHTML += "<p>This calculator is coming soon.</p>";
+    container.innerHTML += `
+
+    <div class="calculator">
+
+        <input
+            id="calcDisplay"
+            type="text"
+            placeholder="0"
+            readonly>
+
+        <div class="calcGrid">
+
+            <button onclick="press('7')">7</button>
+            <button onclick="press('8')">8</button>
+            <button onclick="press('9')">9</button>
+            <button onclick="press('/')">÷</button>
+
+            <button onclick="press('4')">4</button>
+            <button onclick="press('5')">5</button>
+            <button onclick="press('6')">6</button>
+            <button onclick="press('*')">×</button>
+
+            <button onclick="press('1')">1</button>
+            <button onclick="press('2')">2</button>
+            <button onclick="press('3')">3</button>
+            <button onclick="press('-')">−</button>
+
+            <button onclick="press('0')">0</button>
+            <button onclick="press('.')">.</button>
+            <button onclick="calculate()">=</button>
+            <button onclick="press('+')">+</button>
+
+            <button onclick="clearDisplay()">C</button>
+
+        </div>
+
+    </div>
+
+    `;
 
 }
-
 function renderBMICalculator(container){
 
     container.innerHTML += "<p>This calculator is coming soon.</p>";
@@ -56,5 +93,34 @@ function renderTemperatureConverter(container){
 function renderPasswordGenerator(container){
 
     container.innerHTML += "<p>This tool is coming soon.</p>";
+
+}
+function press(value){
+
+    document.getElementById("calcDisplay").value += value;
+
+}
+
+function clearDisplay(){
+
+    document.getElementById("calcDisplay").value = "";
+
+}
+
+function calculate(){
+
+    const display = document.getElementById("calcDisplay");
+
+    try{
+
+        display.value = Function("return " + display.value)();
+
+    }
+
+    catch{
+
+        display.value = "Error";
+
+    }
 
 }

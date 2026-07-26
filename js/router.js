96a8445cba2c3ba42475c69cf52b2dbe9e7f2630
dishanchowdhury111-creator <dishanchowdhury111@gmail.com>
2026-07-26 11:22:09 +0530

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,15 @@
+// ===============================
+// USOBIT ROUTER
+// ===============================
 
+function getParameter(name) {
+    return new URLSearchParams(window.location.search).get(name);
+}
+
+function getTool(id) {
+    return TOOLS.find(tool => tool.id === id);
+}
+
+function getCategory(name) {
+    return DATABASE[name] || [];
+}

31c2cf5c1bbe9313f0fea18b91865462afd198c3
dishanchowdhury111-creator <dishanchowdhury111@gmail.com>
2026-07-26 11:22:27 +0530

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -1,1 +1,60 @@
+// ======================================
+// USOBIT TOOL ENGINE
+// ======================================
 
+const TOOL_FUNCTIONS = {
+
+    "scientific-calculator": renderScientificCalculator,
+
+    "bmi-calculator": renderBMICalculator,
+
+    "age-calculator": renderAgeCalculator,
+
+    "length-converter": renderLengthConverter,
+
+    "temperature-converter": renderTemperatureConverter,
+
+    "password-generator": renderPasswordGenerator
+
+};
+
+
+// ======================================
+// PLACEHOLDER FUNCTIONS
+// ======================================
+
+function renderScientificCalculator(container){
+
+    container.innerHTML += "<p>This calculator is coming soon.</p>";
+
+}
+
+function renderBMICalculator(container){
+
+    container.innerHTML += "<p>This calculator is coming soon.</p>";
+
+}
+
+function renderAgeCalculator(container){
+
+    container.innerHTML += "<p>This calculator is coming soon.</p>";
+
+}
+
+function renderLengthConverter(container){
+
+    container.innerHTML += "<p>This converter is coming soon.</p>";
+
+}
+
+function renderTemperatureConverter(container){
+
+    container.innerHTML += "<p>This converter is coming soon.</p>";
+
+}
+
+function renderPasswordGenerator(container){
+
+    container.innerHTML += "<p>This tool is coming soon.</p>";
+
+}

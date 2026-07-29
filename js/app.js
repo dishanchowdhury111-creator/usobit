9f5bf8f5cc8327f91963646e0143e76f56db151b
dishanchowdhury111-creator <dishanchowdhury111@gmail.com>
2026-07-29 16:38:13 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('Usobit loaded');
+const container = document.getElementById("featuredTools");
+
+if (container) {
+    TOOLS.forEach(tool => {
+        container.innerHTML += `
+            <div class="card">
+                <h3>${tool.icon} ${tool.name}</h3>
+                <p>${tool.description}</p>
+            </div>
+        `;
+    });
+}
